9f56787cd6c0f72d5ea195d64c0c41316970615a
objectiser <gary@pi4tech.com>
2011-03-11 12:58:20 +0000

    Update script to ask maven to check for updates

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,5 +2,5 @@
 # Hudson script for publishing the bundles to nexus
 
 if [ "$RELEASE" == "No" ]; then
-	mvn clean deploy -Dmaven.repo.local=${WORKSPACE}/m2-repository
+	mvn clean deploy -U -Dmaven.repo.local=${WORKSPACE}/m2-repository
 fi
