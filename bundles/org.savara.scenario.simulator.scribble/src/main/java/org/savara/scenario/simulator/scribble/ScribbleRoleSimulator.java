9e067de2556cf6cfe494a48b734fd80205802650
objectiser <gary@pi4tech.com>
2012-02-02 11:58:16 +0000

    SAVARA-280 added initial support for scribble based simulation, based on both global and local protocols

--- bundles/org.savara.scenario.simulator.scribble/src/main/java/org/savara/scenario/simulator/scribble/ScribbleRoleSimulator.java
+++ bundles/org.savara.scenario.simulator.scribble/src/main/java/org/savara/scenario/simulator/scribble/ScribbleRoleSimulator.java
@@ -17,23 +17,35 @@
  */
 package org.savara.scenario.simulator.scribble;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import org.savara.monitor.ConversationId;
-import org.savara.monitor.Message;
-import org.savara.monitor.Monitor;
+import org.savara.protocol.util.ProtocolServices;
 import org.savara.scenario.model.Event;
 import org.savara.scenario.model.MessageEvent;
-import org.savara.scenario.model.Parameter;
 import org.savara.scenario.model.ReceiveEvent;
 import org.savara.scenario.model.Role;
-import org.savara.scenario.model.Scenario;
 import org.savara.scenario.model.SendEvent;
 import org.savara.scenario.simulation.RoleSimulator;
 import org.savara.scenario.simulation.SimulationContext;
 import org.savara.scenario.simulation.SimulationHandler;
 import org.savara.scenario.simulation.SimulationModel;
+import org.scribble.common.logging.CachedJournal;
+import org.scribble.common.resource.ByteArrayContent;
+import org.scribble.common.resource.Content;
+import org.scribble.protocol.DefaultProtocolContext;
+import org.scribble.protocol.export.monitor.MonitorProtocolExporter;
+import org.scribble.protocol.model.ProtocolModel;
+import org.scribble.protocol.monitor.DefaultSession;
+import org.scribble.protocol.monitor.MonitorContext;
+import org.scribble.protocol.monitor.ProtocolMonitor;
+import org.scribble.protocol.monitor.ProtocolMonitorFactory;
+import org.scribble.protocol.monitor.Result;
+import org.scribble.protocol.monitor.Session;
+import org.scribble.protocol.monitor.model.Description;
+import org.scribble.protocol.monitor.util.MonitorModelUtil;
 
 /**
  * This class provides the scribble implementation of the Role Simulator interface.
@@ -41,129 +53,209 @@ import org.savara.scenario.simulation.SimulationModel;
  */
 public class ScribbleRoleSimulator implements RoleSimulator {
 
-	private Monitor m_monitor=null;
+	private static Logger LOG=Logger.getLogger(ScribbleRoleSimulator.class.getName());
+	
+	private static final String PROTOCOL_SIMULATOR = "Protocol simulator";
+
+	private ProtocolMonitor _monitor=ProtocolMonitorFactory.createProtocolMonitor();
+	private MonitorProtocolExporter _exporter=new MonitorProtocolExporter();
+	private MonitorContext _context=new MonitorContextImpl();
 	
 	/**
-	 * This method sets the monitor used to simulate the scenario.
-	 * 
-	 * @param monitor The monitor
+	 * {@inheritDoc}
 	 */
-	public void setMonitor(Monitor monitor) {
-		m_monitor = monitor;
+	public String getName() {
+		return PROTOCOL_SIMULATOR;
 	}
-	
+
 	/**
-	 * This method simulates the scenario against the pre-configured
-	 * monitor. Results from the simulation are notified to the
-	 * supplied simulation handler.
-	 * 
-	 * @param scenario The scenario to be simulated
-	 * @param handler The callback to notify of the simulation results
+	 * {@inheritDoc}
 	 */
-	public void simulate(Scenario scenario, SimulationHandler handler) {
-		
-		ConversationId cid=new ConversationId(UUID.randomUUID().toString());
-		
-		simulateEvents(cid, scenario.getEvent(), handler);
+	public void initialize(SimulationContext context) throws Exception {
+		context.getProperties().put(Session.class.getName(), _monitor.createSession(_context,
+					(Description)context.getModel(), DefaultSession.class));
 	}
-	
+
 	/**
-	 * This method simulates a list of events, associated with a scenario.
-	 * 
-	 * @param cid The conversation instance id
-	 * @param events The list of events
-	 * @param handler The handler
+	 * {@inheritDoc}
 	 */
-	protected void simulateEvents(ConversationId cid,
-					java.util.List<Event> events, SimulationHandler handler) {
+	public boolean isSupported(SimulationModel model) {
+		Content content=new ByteArrayContent(model.getName(), null);
 		
-		for (Event event : events) {
-			if (event instanceof SendEvent) {
-				handleSendEvent(cid, (SendEvent)event, handler);
-			} else if (event instanceof ReceiveEvent) {
-				handleReceiveEvent(cid, (ReceiveEvent)event, handler);
+		return (ProtocolServices.getParserManager().isParserAvailable(content));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Object getModel(SimulationModel model) {
+		Object ret=null;
+		
+		try {
+			java.io.InputStream is=model.getContents();
+
+			byte[] b=new byte[is.available()];
+			is.read(b);
+			
+			is.close();
+			
+			Content content=new ByteArrayContent(model.getName(), b);
+			
+			CachedJournal journal=new CachedJournal();
+			
+			ret = ProtocolServices.getParserManager().parse(null, content, journal);
+		} catch(Exception e) {
+			LOG.log(Level.SEVERE, "Failed to get model", e);
+		}
+		
+		return (ret);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<Role> getModelRoles(Object model) {
+		if (model instanceof ProtocolModel) {
+			java.util.List<Role> ret=new java.util.Vector<Role>();
+			
+			if (((ProtocolModel)model).isLocated()) {
+				Role role=new Role();
+				role.setName(((ProtocolModel)model).getProtocol().getLocatedRole().getName());
+				ret.add(role);
 			} else {
-				handler.unexpected(event);
+				for (org.scribble.protocol.model.Role r : ((ProtocolModel)model).getRoles()) {
+					Role role=new Role();
+					role.setName(r.getName());
+					ret.add(role);
+				}
+			}
+			
+			return (ret);
+		}
+		
+		return (Collections.emptyList());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Object getModelForRole(Object model, Role role) {
+		Object ret=null;
+		
+		DefaultProtocolContext context=
+				new DefaultProtocolContext(ProtocolServices.getParserManager(),
+						null);
+
+		CachedJournal journal=new CachedJournal();
+
+		ProtocolModel local=null;
+		
+		if (((ProtocolModel)model).isLocated()) {
+			if (((ProtocolModel)model).getProtocol().getLocatedRole().getName().equals(role.getName())) {
+				local = (ProtocolModel)model;
+			}
+		} else {
+			local = ProtocolServices.getProtocolProjector().project(context,
+					(ProtocolModel)model, new org.scribble.protocol.model.Role(role.getName()),
+								journal);
+		}
+
+		if (local != null) {
+			try {
+				// Convert protocol model to monitoring description
+				java.io.ByteArrayOutputStream os=new java.io.ByteArrayOutputStream();
+				
+				_exporter.export(local, journal, os);
+				
+				os.close();
+				
+				if (journal.hasErrors()) {
+					LOG.severe("Errors detected when exporting protocol '"+
+									local+"' to monitorable description");
+				} else {
+					java.io.InputStream is=new java.io.ByteArrayInputStream(os.toByteArray());
+					
+					ret = MonitorModelUtil.deserialize(is);
+					
+					is.close();
+				}
+			} catch(Exception e) {
+				LOG.log(Level.SEVERE, "Failed to export monitor description for located protocol model", e);
+			}
+		}
+		
+		return (ret);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void onEvent(SimulationContext context, Event event,
+							SimulationHandler handler) {
+		
+		if (event instanceof MessageEvent) {
+			MessageEvent me=(MessageEvent)event;
+			
+			if (me.getParameter().size() == 1) {
+				String type=me.getParameter().get(0).getType();
+				String path=me.getParameter().get(0).getValue();
+				
+				java.io.Serializable value=null;
+				
+				if (path != null) {
+					try {
+						java.io.InputStream is=context.getResource(path);
+						
+						byte[] b=new byte[is.available()];
+						
+						is.read(b);
+						
+						is.close();
+						
+						// Assume value is text
+						value = new String(b);
+						
+					} catch(Exception e) {
+						handler.error("Failed to obtain message value", event, e);
+					}
+				}
+				
+				try {
+					Message mesg=new Message();
+					
+					mesg.setOperator(((MessageEvent) event).getOperationName());
+					mesg.getTypes().add(type);
+					mesg.getValues().add((String)value);
+					
+					Result result=null;
+					
+					if (event instanceof SendEvent) {
+						result = _monitor.messageSent(_context, (Description)context.getModel(),
+									(Session)context.getProperties().get(Session.class.getName()), mesg);
+					} else if (event instanceof ReceiveEvent) {
+						result = _monitor.messageReceived(_context, (Description)context.getModel(),
+								(Session)context.getProperties().get(Session.class.getName()), mesg);
+					}
+					
+					if (result.isValid()) {
+						handler.processed(event);
+					} else {
+						handler.unexpected(event);
+					}
+				} catch(Exception e) {
+					handler.error("Failed to simulate message", event, e);
+				}
+			} else {
+				handler.error("Cannot simulate event as does not have single parameter",
+								event, null);
 			}
 		}
 	}
-	
-	protected Message getMessageForEvent(MessageEvent event) {
-		Message mesg=new Message();
-		mesg.setOperator(event.getOperationName());
-		
-		for (Parameter p : event.getParameter()) {
-			mesg.getTypes().add(p.getType());
-			mesg.getValues().add(p.getValue());
-		}
-		
-		return(mesg);
-	}
-	
-	protected void handleSendEvent(ConversationId cid,
-					SendEvent event, SimulationHandler handler) {
-		try {
-			Message mesg=getMessageForEvent(event);
-			
-			mesg.setSourceEndpointType(((Role)event.getRole()).getName());
 
-			m_monitor.process(null, cid, mesg);
-		} catch(Exception e) {
-			handler.error("Failed when handling send event", event, e);
-		}
-	}
-	
-	protected void handleReceiveEvent(ConversationId cid,
-					ReceiveEvent event, SimulationHandler handler) {
-		try {
-			Message mesg=getMessageForEvent(event);
-			
-			mesg.setDestinationEndpointType(((Role)event.getRole()).getName());
-
-			m_monitor.process(null, cid, mesg);
-		} catch(Exception e) {
-			handler.error("Failed when handling receive event", event, e);
-		}		
-	}
-
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void initialize(SimulationContext context) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public boolean isSupported(SimulationModel model) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public Object getModel(SimulationModel model) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public List<Role> getModelRoles(Object model) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Object getModelForRole(Object model, Role role) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void onEvent(SimulationContext context, Event event,
-			SimulationHandler handler) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	/**
+	 * {@inheritDoc}
+	 */
 	public void close(SimulationContext context) throws Exception {
-		// TODO Auto-generated method stub
 		
 	}
 }
--- bundles/org.savara.scenario.simulator.scribble/src/test/java/org/savara/scenario/simulator/scribble/ScribbleRoleSimulatorTest.java
+++ bundles/org.savara.scenario.simulator.scribble/src/test/java/org/savara/scenario/simulator/scribble/ScribbleRoleSimulatorTest.java
@@ -19,8 +19,20 @@ package org.savara.scenario.simulator.scribble;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
+import org.savara.scenario.model.Role;
+import org.savara.scenario.model.Scenario;
+import org.savara.scenario.simulation.DefaultSimulationContext;
+import org.savara.scenario.simulation.RoleSimulator;
+import org.savara.scenario.simulation.ScenarioSimulator;
+import org.savara.scenario.simulation.ScenarioSimulatorFactory;
+import org.savara.scenario.simulation.SimulationContext;
 import org.savara.scenario.simulation.SimulationModel;
+import org.savara.scenario.util.ScenarioModelUtil;
+import org.scribble.protocol.model.ProtocolModel;
+import org.scribble.protocol.monitor.model.Description;
 
 public class ScribbleRoleSimulatorTest {
 
@@ -28,22 +40,336 @@ public class ScribbleRoleSimulatorTest {
 	public void testGetSupportedModel() {
 		ScribbleRoleSimulator sim=new ScribbleRoleSimulator();
 		
-		/*
+		String filename="monitor/models/PurchaseGoods.spr";
+		
+		java.io.InputStream is=
+			ClassLoader.getSystemResourceAsStream(filename);
+		
 		try {
-			SimulationModel simmodel=new SimulationModel("/qasimmodels/bpmn/Purchasing.bpmn",null);
+			SimulationModel simmodel=new SimulationModel(filename, is);
 			
 			Object model=sim.getModel(simmodel);
 			
 			if (model == null) {
 				fail("Model is null");
-//			} else if ((model instanceof Node) == false) {
-//				fail("Model is not a Tuscany SCA node");
+			} else if ((model instanceof ProtocolModel) == false) {
+				fail("Model is not a ProtocolModel");
 			}
 		} catch(Exception e) {
 			fail("Exception occurred: "+e);
 		}
-		*/
+
 	}
 
+	@Test
+	public void testGetModelRoles() {
+		
+		String filename="monitor/models/PurchaseGoods.spr";
+		
+		java.io.InputStream is=
+			ClassLoader.getSystemResourceAsStream(filename);
+		
+		try {
+			SimulationModel sm=new SimulationModel(filename, is);
+			
+			ScribbleRoleSimulator sim=new ScribbleRoleSimulator();
+			
+			Object model=sim.getModel(sm);
+			
+			java.util.List<Role> roles=sim.getModelRoles(model);
+			
+			if (roles.size() != 4) {
+				fail("Incorrect number of roles, expecting 4, but got: "+roles.size());
+			}
+			
+			Role r1=new Role();
+			r1.setName("Buyer");
+			
+			Role r2=new Role();
+			r2.setName("CreditAgency");
+			
+			Role r3=new Role();
+			r3.setName("Store");
+			
+			Role r4=new Role();
+			r4.setName("Logistics");
+			
+			if (roles.get(0).getName().equals(r1.getName()) == false) {
+				fail("Role '"+r1.getName()+"' not found");
+			}
+			
+			if (roles.get(1).getName().equals(r2.getName()) == false) {
+				fail("Role '"+r2.getName()+"' not found");
+			}
+			
+			if (roles.get(2).getName().equals(r3.getName()) == false) {
+				fail("Role '"+r3.getName()+"' not found");
+			}
+			
+			if (roles.get(3).getName().equals(r4.getName()) == false) {
+				fail("Role '"+r4.getName()+"' not found");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Failed to get model roles: "+e);
+		}
+	}
+
+	@Test
+	public void testGetModelRolesForLocalModel() {
+		
+		String filename="monitor/models/PurchaseGoods@Store.spr";
+		
+		java.io.InputStream is=
+			ClassLoader.getSystemResourceAsStream(filename);
+		
+		try {
+			SimulationModel sm=new SimulationModel(filename, is);
+			
+			ScribbleRoleSimulator sim=new ScribbleRoleSimulator();
+			
+			Object model=sim.getModel(sm);
+			
+			java.util.List<Role> roles=sim.getModelRoles(model);
+			
+			if (roles.size() != 1) {
+				fail("Incorrect number of roles, expecting 1, but got: "+roles.size());
+			}
+			
+			Role r1=new Role();
+			r1.setName("Store");
+			
+			if (roles.get(0).getName().equals(r1.getName()) == false) {
+				fail("Role '"+r1.getName()+"' not found");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Failed to get model roles: "+e);
+		}
+	}
+
+	@Test
+	public void testGetModelForRoleStore() {
+		
+		String filename="monitor/models/PurchaseGoods.spr";
+		
+		java.io.InputStream is=
+			ClassLoader.getSystemResourceAsStream(filename);
+		
+		try {
+			SimulationModel sm=new SimulationModel(filename, is);
+			
+			ScribbleRoleSimulator sim=new ScribbleRoleSimulator();
+			
+			Object model=sim.getModel(sm);
+			
+			Role role=new Role();
+			role.setName("Store");
+			
+			Object localModel=sim.getModelForRole(model, role);
+			
+			if (localModel == null) {
+				fail("Local Model is null");
+			} else if ((localModel instanceof Description) == false) {
+				fail("Local Model is not a Protocol Monitor Description");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Failed to get model roles: "+e);
+		}
+	}
+
+	@Test
+	public void testGetModelForRoleStoreFromLocalModel() {
+		
+		String filename="monitor/models/PurchaseGoods@Store.spr";
+		
+		java.io.InputStream is=
+			ClassLoader.getSystemResourceAsStream(filename);
+		
+		try {
+			SimulationModel sm=new SimulationModel(filename, is);
+			
+			ScribbleRoleSimulator sim=new ScribbleRoleSimulator();
+			
+			Object model=sim.getModel(sm);
+			
+			Role role=new Role();
+			role.setName("Store");
+			
+			Object localModel=sim.getModelForRole(model, role);
+			
+			if (localModel == null) {
+				fail("Local Model is null");
+			} else if ((localModel instanceof Description) == false) {
+				fail("Local Model is not a Protocol Monitor Description");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Failed to get model roles: "+e);
+		}
+	}
+
+	@Test
+	public void testSimulatePurchaseGoods_SuccessfulPurchase() {
+		
+		String filename="monitor/models/PurchaseGoods.spr";
+		
+		java.io.InputStream is=
+			ClassLoader.getSystemResourceAsStream(filename);
+		
+		try {
+			SimulationModel sm=new SimulationModel(filename, is);
+			
+			ScribbleRoleSimulator rsim=new ScribbleRoleSimulator();
+			
+			Object model=rsim.getModel(sm);
+			
+			java.util.List<Role> roles=rsim.getModelRoles(model);
+			
+			// Load the scenario
+			String scenarioFile="scenarios/purchasegoods/SuccessfulPurchase.scn";
+			
+			java.net.URL url=ClassLoader.getSystemResource(scenarioFile);
+			
+			java.io.InputStream sis=url.openStream();
+			
+			Scenario scenario=ScenarioModelUtil.deserialize(sis);
+			
+			sis.close();
+
+			// Create the simulator
+			ScenarioSimulator ssim=ScenarioSimulatorFactory.getScenarioSimulator();
+			
+			TestSimulationHandler handler=new TestSimulationHandler();
+			
+			java.util.Map<Role,RoleSimulator> roleSimulators=new java.util.HashMap<Role,RoleSimulator>();
+			java.util.Map<Role,SimulationContext> contexts=new java.util.HashMap<Role,SimulationContext>();
+			
+			// Reorder roles
+			roles.add(1, roles.remove(2));
+			
+			for (int i=0; i < roles.size(); i++) {
+				roleSimulators.put(scenario.getRole().get(i), rsim);
+				
+				DefaultSimulationContext context=new DefaultSimulationContext(new File(url.getFile()));
+				
+				context.setModel(rsim.getModelForRole(model, roles.get(i)));
+				
+				rsim.initialize(context);
+				
+				contexts.put(scenario.getRole().get(i), context);
+			}
+			
+			ssim.simulate(scenario, roleSimulators, contexts, handler);
+			
+			if (handler.getProcessedEvents().size() != 12) {
+				fail("Twelve events were not processed: "+handler.getProcessedEvents().size());
+			}
+			
+			if (handler.getUnexpectedEvents().size() != 0) {
+				fail("Should be no unexpected events");
+			}
+			
+			if (handler.getErrorEvents().size() != 0) {
+				fail("Should be no error events");
+			}
+			
+			if (handler.getNoSimulatorEvents().size() != 0) {
+				fail("Should be no 'no simulator' events");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Failed to get model roles: "+e);
+		}
+	}
+
+
+	@Test
+	public void testSimulatePurchaseGoodsAtStore_SuccessfulPurchase() {
+		
+		String filename="monitor/models/PurchaseGoods@Store.spr";
+		
+		java.io.InputStream is=
+			ClassLoader.getSystemResourceAsStream(filename);
+		
+		try {
+			SimulationModel sm=new SimulationModel(filename, is);
+			
+			ScribbleRoleSimulator rsim=new ScribbleRoleSimulator();
+			
+			Object model=rsim.getModel(sm);
+			
+			java.util.List<Role> roles=rsim.getModelRoles(model);
+			
+			if (roles.size() != 1) {
+				fail("Expecting 1 role: "+roles.size());
+			}
+			
+			// Load the scenario
+			String scenarioFile="scenarios/purchasegoods/SuccessfulPurchase.scn";
+			
+			java.net.URL url=ClassLoader.getSystemResource(scenarioFile);
+			
+			java.io.InputStream sis=url.openStream();
+			
+			Scenario scenario=ScenarioModelUtil.deserialize(sis);
+			
+			sis.close();
+
+			// Create the simulator
+			ScenarioSimulator ssim=ScenarioSimulatorFactory.getScenarioSimulator();
+			
+			TestSimulationHandler handler=new TestSimulationHandler();
+			
+			java.util.Map<Role,RoleSimulator> roleSimulators=new java.util.HashMap<Role,RoleSimulator>();
+			java.util.Map<Role,SimulationContext> contexts=new java.util.HashMap<Role,SimulationContext>();
+			
+			Role storeRole=null;
+			for (Role r : scenario.getRole()) {
+				if (r.getName().equals("Store")) {
+					storeRole = r;
+					break;
+				}
+			}
+			
+			roleSimulators.put(storeRole, rsim);
+				
+			DefaultSimulationContext context=new DefaultSimulationContext(new File(url.getFile()));
+				
+			context.setModel(rsim.getModelForRole(model, roles.get(0)));
+				
+			rsim.initialize(context);
+				
+			contexts.put(storeRole, context);
+			
+			ssim.simulate(scenario, roleSimulators, contexts, handler);
+			
+			if (handler.getProcessedEvents().size() != 6) {
+				fail("Twelve events were not processed: "+handler.getProcessedEvents().size());
+			}
+			
+			if (handler.getUnexpectedEvents().size() != 0) {
+				fail("Should be no unexpected events");
+			}
+			
+			if (handler.getErrorEvents().size() != 0) {
+				fail("Should be no error events");
+			}
+			
+			if (handler.getNoSimulatorEvents().size() != 6) {
+				fail("Should be no 'no simulator' events");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Failed to get model roles: "+e);
+		}
+	}
 
 }
