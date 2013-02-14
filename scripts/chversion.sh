dfb5b910ff1419f1a74995689c4e2b372ea3a8e4
objectiser <gary@pi4tech.com>
2013-02-14 11:38:49 +0000

    Update version to 2.1.0.Final

--- scripts/chversion.sh
+++ scripts/chversion.sh
@@ -8,13 +8,3 @@ TESTTOVER=`echo $TOVER | egrep '*-SNAPSHOT'`
 
 perl -pi -e "s/$FROMVER/$TOVER/g" `find . -name pom.xml`
 
-if [ "$TESTFROMVER" == "$FROMVER" ]; then
-	FROMVER=`echo $FROMVER | sed 's/-SNAPSHOT/.SNAPSHOT/g'`
-fi
-
-if [ "$TESTTOVER" == "$TOVER" ]; then
-	TOVER=`echo $TOVER | sed 's/-SNAPSHOT/.SNAPSHOT/g'`
-fi
-
-perl -pi -e "s/$FROMVER/$TOVER/g" `find . -name MANIFEST.MF`
-
